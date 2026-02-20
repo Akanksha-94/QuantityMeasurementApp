0c25d2aa1ded1db8b2c3718f6acfb0baa310fc61
Akanksha-94 <akankshabhardwaj447@gmail.com>
2026-02-20 11:48:59 +0530

    [Akanksha Bhardwaj] Add . UC2- Feet Measurement

--- src/main/java/com/akanksha/QuantityMeasurementApp.java
+++ src/main/java/com/akanksha/QuantityMeasurementApp.java
@@ -31,28 +31,70 @@ public class QuantityMeasurementApp {
     }
   }
 
+  public static class Inches {
+    private final double value;
+
+    public Inches(double value) {
+      this.value = value;
+    }
+
+    public double getValue() {
+      return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null || this.getClass() != obj.getClass()) {
+        return false;
+      }
+      Inches inches = (Inches) obj;
+      return Double.compare(this.value, inches.value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+      return Double.hashCode(value);
+    }
+  }
+
   public static void main(String[] args) {
+    // Feet equality checks
+    System.out.println("=== FEET EQUALITY CHECKS ===");
     Feet feet1 = new Feet(1.0);
     Feet feet2 = new Feet(1.0);
 
-    System.out.println("Feet 1: " + feet1.getValue() + " ft");
-    System.out.println("Feet 2: " + feet2.getValue() + " ft");
-    System.out.println("Are they equal? " + feet1.equals(feet2));
+    System.out.println("Input: " + feet1.getValue() + " ft and " + feet2.getValue() + " ft");
+    System.out.println("Output: " + (feet1.equals(feet2) ? "Equal (true)" : "Not Equal (false)"));
 
     Feet feet3 = new Feet(2.0);
-    System.out.println("\nFeet 1: " + feet1.getValue() + " ft");
-    System.out.println("Feet 3: " + feet3.getValue() + " ft");
-    System.out.println("Are they equal? " + feet1.equals(feet3));
+    System.out.println("\nInput: " + feet1.getValue() + " ft and " + feet3.getValue() + " ft");
+    System.out.println("Output: " + (feet1.equals(feet3) ? "Equal (true)" : "Not Equal (false)"));
 
-    System.out.println("\nFeet 1: " + feet1.getValue() + " ft");
-    System.out.println("Null value: null");
-    System.out.println("Are they equal? " + feet1.equals(null));
+    System.out.println("\nInput: " + feet1.getValue() + " ft and null");
+    System.out.println("Output: " + (feet1.equals(null) ? "Equal (true)" : "Not Equal (false)"));
 
-    System.out.println("\nFeet 1: " + feet1.getValue() + " ft");
-    System.out.println("String value: \"1.0\"");
-    System.out.println("Are they equal? " + feet1.equals("1.0"));
+    System.out.println("\nInput: " + feet1.getValue() + " ft and \"1.0\" (String)");
+    System.out.println("Output: " + (feet1.equals("1.0") ? "Equal (true)" : "Not Equal (false)"));
 
-    System.out.println("\nFeet 1 with itself:");
-    System.out.println("Are they equal? " + feet1.equals(feet1));
+    // Inches equality checks
+    System.out.println("\n=== INCHES EQUALITY CHECKS ===");
+    Inches inches1 = new Inches(1.0);
+    Inches inches2 = new Inches(1.0);
+
+    System.out.println("Input: " + inches1.getValue() + " inch and " + inches2.getValue() + " inch");
+    System.out.println("Output: " + (inches1.equals(inches2) ? "Equal (true)" : "Not Equal (false)"));
+
+    Inches inches3 = new Inches(2.0);
+    System.out.println("\nInput: " + inches1.getValue() + " inch and " + inches3.getValue() + " inch");
+    System.out.println("Output: " + (inches1.equals(inches3) ? "Equal (true)" : "Not Equal (false)"));
+
+    System.out.println("\nInput: " + inches1.getValue() + " inch and null");
+    System.out.println("Output: " + (inches1.equals(null) ? "Equal (true)" : "Not Equal (false)"));
+
+    System.out.println("\nInput: " + inches1.getValue() + " inch and \"1.0\" (String)");
+    System.out.println("Output: " + (inches1.equals("1.0") ? "Equal (true)" : "Not Equal (false)"));
   }
 }
--- src/test/java/com/akanksha/AppTest.java
+++ src/test/java/com/akanksha/AppTest.java
@@ -7,48 +7,57 @@ import org.junit.Test;
 
 public class AppTest {
 
+    // Feet test variables
     private QuantityMeasurementApp.Feet feet1;
     private QuantityMeasurementApp.Feet feet2;
+
+    // Inches test variables
+    private QuantityMeasurementApp.Inches inches1;
+    private QuantityMeasurementApp.Inches inches2;
 
     @Before
     public void setUp() {
         feet1 = new QuantityMeasurementApp.Feet(1.0);
         feet2 = new QuantityMeasurementApp.Feet(1.0);
+        inches1 = new QuantityMeasurementApp.Inches(1.0);
+        inches2 = new QuantityMeasurementApp.Inches(1.0);
     }
 
+    // ===== FEET TEST CASES =====
+
     @Test
-    public void testEquality_SameValue() {
+    public void testFeetEquality_SameValue() {
         assertTrue(feet1.equals(feet2));
     }
 
     @Test
-    public void testEquality_DifferentValue() {
+    public void testFeetEquality_DifferentValue() {
         QuantityMeasurementApp.Feet feet3 = new QuantityMeasurementApp.Feet(2.0);
         assertFalse(feet1.equals(feet3));
     }
 
     @Test
-    public void testEquality_NullComparison() {
+    public void testFeetEquality_NullComparison() {
         assertFalse(feet1.equals(null));
     }
 
     @Test
-    public void testEquality_NonNumericInput() {
+    public void testFeetEquality_NonNumericInput() {
         assertFalse(feet1.equals("1.0"));
     }
 
     @Test
-    public void testEquality_SameReference() {
+    public void testFeetEquality_SameReference() {
         assertTrue(feet1.equals(feet1));
     }
 
     @Test
-    public void testEquality_ReflexiveProperty() {
+    public void testFeetEquality_ReflexiveProperty() {
         assertTrue(feet1.equals(feet1));
     }
 
     @Test
-    public void testEquality_SymmetricProperty() {
+    public void testFeetEquality_SymmetricProperty() {
         QuantityMeasurementApp.Feet feetA = new QuantityMeasurementApp.Feet(5.0);
         QuantityMeasurementApp.Feet feetB = new QuantityMeasurementApp.Feet(5.0);
 
@@ -57,7 +66,7 @@ public class AppTest {
     }
 
     @Test
-    public void testEquality_TransitiveProperty() {
+    public void testFeetEquality_TransitiveProperty() {
         QuantityMeasurementApp.Feet feetA = new QuantityMeasurementApp.Feet(3.0);
         QuantityMeasurementApp.Feet feetB = new QuantityMeasurementApp.Feet(3.0);
         QuantityMeasurementApp.Feet feetC = new QuantityMeasurementApp.Feet(3.0);
@@ -68,9 +77,116 @@ public class AppTest {
     }
 
     @Test
-    public void testEquality_ConsistentProperty() {
+    public void testFeetEquality_ConsistentProperty() {
         assertTrue(feet1.equals(feet2));
         assertTrue(feet1.equals(feet2));
         assertTrue(feet1.equals(feet2));
+    }
+
+    @Test
+    public void testFeetEquality_ZeroValue() {
+        QuantityMeasurementApp.Feet feetZero1 = new QuantityMeasurementApp.Feet(0.0);
+        QuantityMeasurementApp.Feet feetZero2 = new QuantityMeasurementApp.Feet(0.0);
+        assertTrue(feetZero1.equals(feetZero2));
+    }
+
+    @Test
+    public void testFeetEquality_NegativeValue() {
+        QuantityMeasurementApp.Feet feetNeg1 = new QuantityMeasurementApp.Feet(-5.0);
+        QuantityMeasurementApp.Feet feetNeg2 = new QuantityMeasurementApp.Feet(-5.0);
+        assertTrue(feetNeg1.equals(feetNeg2));
+    }
+
+    @Test
+    public void testFeetEquality_DecimalPrecision() {
+        QuantityMeasurementApp.Feet feetDecimal1 = new QuantityMeasurementApp.Feet(1.5);
+        QuantityMeasurementApp.Feet feetDecimal2 = new QuantityMeasurementApp.Feet(1.5);
+        assertTrue(feetDecimal1.equals(feetDecimal2));
+    }
+
+    // ===== INCHES TEST CASES =====
+
+    @Test
+    public void testInchesEquality_SameValue() {
+        assertTrue(inches1.equals(inches2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        QuantityMeasurementApp.Inches inches3 = new QuantityMeasurementApp.Inches(2.0);
+        assertFalse(inches1.equals(inches3));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        assertFalse(inches1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_NonNumericInput() {
+        assertFalse(inches1.equals("1.0"));
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        assertTrue(inches1.equals(inches1));
+    }
+
+    @Test
+    public void testInchesEquality_ReflexiveProperty() {
+        assertTrue(inches1.equals(inches1));
+    }
+
+    @Test
+    public void testInchesEquality_SymmetricProperty() {
+        QuantityMeasurementApp.Inches inchesA = new QuantityMeasurementApp.Inches(5.0);
+        QuantityMeasurementApp.Inches inchesB = new QuantityMeasurementApp.Inches(5.0);
+
+        assertTrue(inchesA.equals(inchesB));
+        assertTrue(inchesB.equals(inchesA));
+    }
+
+    @Test
+    public void testInchesEquality_TransitiveProperty() {
+        QuantityMeasurementApp.Inches inchesA = new QuantityMeasurementApp.Inches(3.0);
+        QuantityMeasurementApp.Inches inchesB = new QuantityMeasurementApp.Inches(3.0);
+        QuantityMeasurementApp.Inches inchesC = new QuantityMeasurementApp.Inches(3.0);
+
+        assertTrue(inchesA.equals(inchesB));
+        assertTrue(inchesB.equals(inchesC));
+        assertTrue(inchesA.equals(inchesC));
+    }
+
+    @Test
+    public void testInchesEquality_ConsistentProperty() {
+        assertTrue(inches1.equals(inches2));
+        assertTrue(inches1.equals(inches2));
+        assertTrue(inches1.equals(inches2));
+    }
+
+    @Test
+    public void testInchesEquality_ZeroValue() {
+        QuantityMeasurementApp.Inches inchesZero1 = new QuantityMeasurementApp.Inches(0.0);
+        QuantityMeasurementApp.Inches inchesZero2 = new QuantityMeasurementApp.Inches(0.0);
+        assertTrue(inchesZero1.equals(inchesZero2));
+    }
+
+    @Test
+    public void testInchesEquality_NegativeValue() {
+        QuantityMeasurementApp.Inches inchesNeg1 = new QuantityMeasurementApp.Inches(-5.0);
+        QuantityMeasurementApp.Inches inchesNeg2 = new QuantityMeasurementApp.Inches(-5.0);
+        assertTrue(inchesNeg1.equals(inchesNeg2));
+    }
+
+    @Test
+    public void testInchesEquality_DecimalPrecision() {
+        QuantityMeasurementApp.Inches inchesDecimal1 = new QuantityMeasurementApp.Inches(1.5);
+        QuantityMeasurementApp.Inches inchesDecimal2 = new QuantityMeasurementApp.Inches(1.5);
+        assertTrue(inchesDecimal1.equals(inchesDecimal2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClassComparison() {
+        assertFalse(inches1.equals(feet1));
     }
 }
